c15e389b38af7b9a85fc724775977ba66c33317b
inuitwallet <contact@inuit-wallet.co.uk>
2016-08-23 12:55:51 +0100

    adjust routing now that ws methods have changed

--- sleight/routing.py
+++ sleight/routing.py
@@ -1,12 +1,11 @@
 from channels import route
 from consumers.check_trades import check_trades
-from consumers.websockets import ws_connect, ws_receive
+from consumers.websockets import ws_connect, ws_disconnect
 
 channel_routing = [
     # set up web sockets for updating the front end
     route('websocket.connect', ws_connect),
-    route('websocket.receive', ws_receive),
-    #route('websocket.disconnect', ws_disconnect),
+    route('websocket.disconnect', ws_disconnect),
 
     # set a channel per pair to check for trades as orders are added
     route('btc-usnbt', check_trades),
